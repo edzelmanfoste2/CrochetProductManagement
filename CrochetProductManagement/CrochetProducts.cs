c0c6d7c6e0e55eacdf7a97af45cb880a8d935c90
Edzel Manfoste <edzelmanfoste14@gmail.com>
2024-06-26 23:18:52 +0800

    Added the models

---
+++ CrochetProductManagement/CrochetProducts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementModel
+{
+    public class CrochetProducts
+    {
+        public string ID;
+        public string name;
+        public string productType;
+        public string availability;
+        public string description;
+        public string category;
+        public string material;
+        public string size;
+    }
+}
