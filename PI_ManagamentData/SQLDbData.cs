0075181b5967e32755d5069a6edcdf4deaa72651
Christian Caluya <christianlcaluya@iskolarngbayan.pup.edu.ph>
2024-07-18 16:03:36 +0800

    Manfoste, Edzel T. --Connected through VM server, database

--- PI_ManagamentData/SQLDbData.cs
+++ PI_ManagamentData/SQLDbData.cs
@@ -11,19 +11,26 @@ namespace ProductInventoryManagementModel
 {
     public class SQLDbData
     {
-        string connectionString
-        = "Data Source =MANFOSTE\\SQLEXPRESS01; Initial Catalog = CrochetManagement; Integrated Security = True;";
+        //string connectionString
+        //= "Data Source =MANFOSTE\\SQLEXPRESS01; Initial Catalog = CrochetManagement; Integrated Security = True;";
+
+        //  string connectionString
+        //= "Data Source =ROSWELL\\SQLEXPRESS01; Initial Catalog = CrochetManagement; Integrated Security = True;";
+
+
+        string connection = "Server =tcp:20.2.234.76,1433; Database = CrochetManagement; User Id =sa; Password = Manfoste12345;";
 
         SqlConnection sqlConnection;
 
         public SQLDbData()
         {
-            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection = new SqlConnection(connection);
         }
 
         public List<CrochetProducts> GetProducts()
         {
-            string selectStatement = "SELECT ID, Name, Product_Type, Availability, Description, Category, Material, Size FROM Tbl_CrochetProduct";
+            //string selectStatement = "SELECT ID, Name, ProductType, Availability, Description, Category, Material, Size FROM Tbl_CrochetProduct";
+            string selectStatement = "Select * From Tbl_CrochetProduct;";
 
             SqlCommand selectCommand = new SqlCommand(selectStatement, sqlConnection);
 
@@ -36,7 +43,7 @@ namespace ProductInventoryManagementModel
             {
                 string ID = reader["ID"].ToString();
                 string name = reader["Name"].ToString();
-                string productType = reader["Product_Type"].ToString();
+                string productType = reader["ProductType"].ToString();
                 string availability = reader["Availability"].ToString();
                 string description = reader["Description"].ToString();
                 string category = reader["Category"].ToString();
@@ -50,7 +57,8 @@ namespace ProductInventoryManagementModel
                 readProducts.availability = availability;
                 readProducts.description = description;
                 readProducts.category = category;
-                readProducts.material = size;
+                readProducts.material = material;
+                readProducts.size = size;
                 //readProducts.status = Convert.ToInt16(reader["status"]);
 
                 products.Add(readProducts);
@@ -65,13 +73,13 @@ namespace ProductInventoryManagementModel
         {
             int success;
 
-            string insertStatement = "INSERT INTO Tbl_CrochetProduct VALUES (@ID, @Name, @Product_Type, @Availability, @Description, @Category, @Material, @Size)";
+            string insertStatement = "INSERT INTO Tbl_CrochetProduct VALUES (@ID, @Name, @ProductType, @Availability, @Description, @Category, @Material, @Size)";
 
             SqlCommand insertCommand = new SqlCommand(insertStatement, sqlConnection);
 
             insertCommand.Parameters.AddWithValue("@ID", ID);
             insertCommand.Parameters.AddWithValue("@Name", name);
-            insertCommand.Parameters.AddWithValue("@Product_Type", productType);
+            insertCommand.Parameters.AddWithValue("@ProductType", productType);
             insertCommand.Parameters.AddWithValue("@Availability", availability);
             insertCommand.Parameters.AddWithValue("@Description", description);
             insertCommand.Parameters.AddWithValue("@Category", category);
@@ -92,9 +100,9 @@ namespace ProductInventoryManagementModel
         {
             int success;
 
-            //string updateStatement = $"UPDATE Tbl_CrochetProduct SET ID = @ID, name = @Name, productType = @Product_Type, availability = @Availability, description = @Description, category = @Category, material = @Material, size = @Size WHERE name = @Name";
+            //string updateStatement = $"UPDATE Tbl_CrochetProduct SET ID = @ID, name = @Name, productType = @ProductType, availability = @Availability, description = @Description, category = @Category, material = @Material, size = @Size WHERE name = @Name";
             //string updateStatement =$"update Tbl_CrochetProduct set availability = @Availability where ID = @ID";
-            string updateStatement = $"update Tbl_CrochetProduct set ID = @ID, name = @Name, availability = @Availability where ID = @ID";
+            string updateStatement = $"update Tbl_CrochetProduct set ID = @ID, name = @Name, ProductType = @ProductType, availability = @Availability where ID = @ID";
             SqlCommand updateCommand = new SqlCommand(updateStatement, sqlConnection);
             sqlConnection.Open();
 
@@ -103,7 +111,7 @@ namespace ProductInventoryManagementModel
 
             updateCommand.Parameters.AddWithValue("@ID", ID);
             updateCommand.Parameters.AddWithValue("@Name", name);
-            //updateCommand.Parameters.AddWithValue("@Product_Type", ProductType);
+            updateCommand.Parameters.AddWithValue("@ProductType", ProductType);
             updateCommand.Parameters.AddWithValue("@Availability", availability);
             //updateCommand.Parameters.AddWithValue("@Description", description);
             //updateCommand.Parameters.AddWithValue("@Category", category);
@@ -117,15 +125,15 @@ namespace ProductInventoryManagementModel
             return success;
         }
 
-        public int DeleteProducts(string name)
+        public int DeleteProducts(string ID)
         {
             int success;
 
-            string deleteStatement = $"DELETE FROM Tbl_CrochetProduct WHERE name = @Name";
+            string deleteStatement = $"DELETE FROM Tbl_CrochetProduct WHERE ID = @ID";
             SqlCommand deleteCommand = new SqlCommand(deleteStatement, sqlConnection);
             sqlConnection.Open();
 
-            deleteCommand.Parameters.AddWithValue("@Name", name);
+            deleteCommand.Parameters.AddWithValue("@ID", ID);
 
             success = deleteCommand.ExecuteNonQuery();
 
