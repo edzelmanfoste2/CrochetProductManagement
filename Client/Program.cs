e67b78c70a8e62ac4e1de91821e8562704269de9
Edzel Manfoste <edzelmanfoste14@gmail.com>
2024-10-06 23:31:41 +0800

    MailKit integration, testing of email to be send.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,8 @@
 ﻿using ProductInventoryManagementModel;
 using PI_ManagementServices;
+using MailKit.Net.Smtp;
+using MimeKit;
+
 
 namespace Client
 {
@@ -9,7 +12,7 @@ namespace Client
         {
             //List<CrochetProducts> productsDB = SQLDbData.GetProducts();
 
-            GetProducts getProducts = new GetProducts();
+            /*GetProducts getProducts = new GetProducts();
 
             var avail = getProducts.GetAllProducts();
 
@@ -23,6 +26,41 @@ namespace Client
                 Console.WriteLine(product.category);
                 Console.WriteLine(product.material);
                 Console.WriteLine(product.size);
+            }*/
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Testing1", "do-not-reply@frommynotes.com"));
+            message.To.Add(new MailboxAddress("User", "user@example.com"));
+            message.Subject = "Thanks for suscribing!";
+
+            message.Body = new TextPart("html")
+            {
+                Text = "<h1> Hi! Testing of Email version 2!</h1>" + "<p>Good Day! Thank so much for giving attention to this!</P>"
+                    + "<p><strong>Have Fun!</strong></p>"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect("sandbox.smtp.mailtrap.io", 2525, MailKit.Security.SecureSocketOptions.StartTls);
+
+                    client.Authenticate("690f318e2d42da", "6725be2123c229");
+
+                    client.Send(message);
+
+                    Console.WriteLine("Email successfully sent through MailTrap.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error sending email: (ex.Message)");
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                }
+
+
             }
         }
     }
